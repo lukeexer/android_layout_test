7517cf6721f8e2059becc467197e3507f440ae33
lukeexer <lientuna@gmail.com>
2022-02-23 18:21:21 +0800

    first commit

---
+++ app/src/main/java/luke/exe/mylayout/MainActivity.java
@@ -0,0 +1,15 @@
+package luke.exe.mylayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_frame);
+    }
+}
